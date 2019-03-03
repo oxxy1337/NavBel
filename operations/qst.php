3cb696503052c01c69c2d4e3ef9c647097cbcee2
oxxy1337 <medxmed@bk.ru>
2019-03-03 06:19:19 +0100

    New Structure

--- operations/qst.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once './api/con.php';
- include_once './api/functions.php';
-// instantiate student object
-include_once './object/tables.php';
- $yearr= $_GET['year'];
-$database = new Database();
-$db = $database->getConnection();
-echo grabqst($db,'qst',"*",'year',$yearr);
