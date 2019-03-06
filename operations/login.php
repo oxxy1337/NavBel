d782bf8f01c41e24c23c6d56cd737c658ac4042a
oxxy1337 <medxmed@bk.ru>
2019-03-06 05:18:51 +0100

    login return data

--- operations/login.php
+++ operations/login.php
@@ -9,7 +9,6 @@ $password_from_input = filter($data->password);
 $salt = $glob->grab('users','salt','email',$email_from_input); // getting salt from db for using as key 
 
 $password_from_input= cryptpwd($password_from_input,$salt);
-echo $password_from_input;
 /// getting auth infos from database
 $email_from_db = $glob->grab('users','email','email',$email_from_input);
 $password_from_db = $glob->grab('users','password','email',$email_from_input);
@@ -19,9 +18,28 @@ $password_from_db = $glob->grab('users','password','email',$email_from_input);
 
 if(($email_from_input == $email_from_db) && ($password_from_input == $password_from_db)){
 	/// variables initialisisation
-	echo"yes";
+	
+	$id = 	 $glob->grab('users','id','email',$email_from_input);
+	$fname = $glob->grab('users','fname','email',$email_from_input);
+	$lname = $glob->grab('users','lname','email',$email_from_input);
+	$year =  $glob->grab('users','year','email',$email_from_input);
+	$point = $glob->grab('users','point','email',$email_from_input);
+	$ranks=  $glob->grab('users','ranks','email',$email_from_input);
+	$picture = $glob->grab('users','picture','email',$email_from_input);
+	$date = $glob->grab('users','date','email',$email_from_input);
+	$nbsolved = $glob->grab('users','nbsolved','email',$email_from_input);
+	$currentrank = $glob->grab('users','currentrank','email',$email_from_input);
+	$solvedperday = $glob->grab('users','solvedperday','email',$email_from_input);
+
+	$data = array("reponse"=>"1","id"=>$id,"fname"=>$fname,"lname"=>$lname,"year"=>$year,"point"=>$point,"ranks"=>$ranks,"picture"=>$picture,"date"=>$data,"nbsolved"=>$nbsolved,"currentrank"=>$currentrank,
+		"solvedperday"=>$solvedperday);
+	$data = json_encode($data);
+	echo $data ;
 }else{
-	echo"no";
+
+	$data=array("reponse"=>0);
+	$data=json_encode($data);
+	echo $data;
 }
 
 ?>
