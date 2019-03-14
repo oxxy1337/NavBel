1e409a2e037d8fa9c8a13fde0119a79d5d5f6d4b
oxxy1337 <medxmed@bk.ru>
2019-03-14 23:16:31 +0100

    add html form to password reset

--- operations/mailer.php
+++ operations/mailer.php
@@ -11,12 +11,31 @@ if($mailer =="reset") {
     $rand = rand(100000,999999);
     $subject = "Account details for $fame at Nav Bell";
     $rep=array("reponse"=>1,"code" => $rand);
-    $html = "$fname,<br>
+    $html = "$fname,
+    <div class='email-background' style='background: #eee;padding: 10px;'>
 
-    Thank you for registering at Nav Bell.<br>
-    You may now log in by copying and pasting this code : <font color='red'>$rand</font><br>
-    <br> From $ip <br>
-    <small> Nav Bell Devlopers Team <br> contact@team7.dz</small> ";
+        <div class='pre-header' style='background: #eee;color: #eee;font-size: 5px;'>
+            Almost done! You've one last step to confirm your account. 
+        </div>
+
+        <div class='email-container' style='max-width: 500px;background: white;font-family: sans-serif;margin: 0 auto;overflow: hidden;border-radius: 5px;text-align: center;'>
+
+
+            <img src='logo.jpg' style='max-width: 100%;'>
+            <h1> Hello there! </h1> 
+            <p style='margin: 20px;font-size: 18px;font-weight: 300;color: #666;line-height: 1.5;'> Thank you for registering at NavBel. <br>
+                You may now log in by copying and pasting the code bellow:
+             </p>
+
+            <div class='cta' style='text-decoration: none;display: inline-block;background: #3087F5;color: white;padding: 10px 20px;border-radius: 5px;margin: 20px;'>
+                $rand
+            </div>  
+        </div>  
+        <div class='footer-junk' style='max-width: 500px;background: none;font-family: sans-serif;margin: 0 auto;overflow: hidden;border-radius: 5px;text-align: center;padding: 20px;font-size: 12px;'>
+            contact@team7.dz | <a href='' style='color: #3087F5;text-decoration: none;'>Unsubscribe</a>
+        </div>
+    </div>  
+";
 } elseif ($mailer =="alert") {
     $rep=array("reponse"=>1);
     $email = "m.slamat@esi-sba.dz";
