9dc3ad05fb1fbd61c2b27d097c34f88b51353e0e
oxxy1337 <medxmed@bk.ru>
2019-03-17 18:29:29 +0100

    0x

--- classes/conn.php
+++ classes/conn.php
@@ -8,7 +8,7 @@ coded by m.slamat
     
 class Database{
  
-    
+        // my data is public  don't harm the server or db ....... rak kkbir 
     private $host = "35.203.11.145";
     private $db_name = "navbell";
     private $username = "slamat";
