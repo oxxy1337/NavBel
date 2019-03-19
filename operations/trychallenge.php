ca3eaee05e6aebefe1b3a7a0f497d7193f056402
oxxy1337 <medxmed@bk.ru>
2019-03-19 16:30:32 +0100

    fix errors

--- operations/trychallenge.php
+++ operations/trychallenge.php
@@ -2,6 +2,10 @@
 /*
 coded by m.slamat
 */
+// this operation add userid & challengeid in trychallenge , so next time user can't play this challenge 
 $glob->id=$id;
 $glob->challengeid=$challengeid;
-if($glob->trychallenge()) echo json_encode(array("reponse"=>1)); else echo json_encode(array("reponse"=>1));
+// chack if challenge aviable in challenge list 
+if ($glob->check("challenges","id",$challengeid)==false) die(json_encode(array("reponse"=>-1)));
+// insert in trychallenge table 
+if($glob->trychallenge()) echo json_encode(array("reponse"=>1)); else echo json_encode(array("reponse"=>-1));
