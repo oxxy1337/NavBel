e46e76d14ba76f00779bc2dab5c5eada1a2d7afe
oxxy1337 <medxmed@bk.ru>
2019-07-25 18:36:42 +0200

    update README + add bckp docs

--- backup.sh
+++ backup.sh
@@ -10,7 +10,7 @@ crontab -l > cronbackup ;
 echo "$1 $DIR/backups/init.sh"  >> cronbackup ; 
 crontab cronbackup ;
 rm cronbackup;
-
+docker exec  navbel_api_1 bash chown www-data:www-data /var/www/html/images;
 echo "docker exec  navbel_api_1 bash /var/backups/backup.sh ; mv $DIR/backups/files/* /var/www/html/backups/files/ ;  mv $DIR/backups/db/* /var/www/html/backups/db/ ; chown www-data:www-data /var/www/html/backups ; docker exec  navbel_api_1 php /var/www/html/cron.php" >>  backups/init.sh; 
 mkdir -p /var/www/html/backups/files; mkdir -p /var/www/html/backups/db;
 mv $DIR/backups/.htaccess /var/www/html/backups/.htaccess;
